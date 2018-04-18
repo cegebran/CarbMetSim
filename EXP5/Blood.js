3ac65df36da9f39dde3f60b4d6b982be3eb08a55
pavansailottala <31457772+pavansailottala@users.noreply.github.com>
2018-04-17 19:33:24 -0500

    Add files via upload

---
+++ EXP5/Blood.js
@@ -0,0 +1,244 @@
+//package sim;
+//import java.util.Map.Entry;
+//import org.apache.commons.math3.distribution.PoissonDistribution;
+
+//import enums.BodyOrgan;
+
+
+ class RBCBin {
+        constructor(){
+        var RBCs;
+        var glycatedRBCs;
+    }
+}
+
+class Blood {
+	////////////////////================================================================================
+	//private RBCBin[] AgeBins = new RBCBin[MAXAGE+1];// Aging Bins
+    /////////////////////==========================================================================
+    currentHbA1c() {
+    	var rbcs = 0;
+        var glycated_rbcs = 0;
+        for(var i = 0; i <= Blood.MAXAGE; i++) {
+            rbcs += this.AgeBins[i].RBCs;
+            glycated_rbcs += this.AgeBins[i].glycatedRBCs;
+        }
+        if(rbcs == 0) {
+            console.log("Error in Bloody::currentHbA1c");
+            System.exit(1);
+        }
+        return glycated_rbcs/rbcs;
+    }
+    ///////////////////////////////////=========================================================
+    updateRBCs() {
+    	// will be called once a day
+    	this.bin0--;
+        if( this.bin0 < 0 ) this.bin0 = Blood.MAXAGE;
+        //New RBCs take birth
+        this.AgeBins[bin0].RBCs = this.rbcBirthRate_;
+        this.AgeBins[bin0].glycatedRBCs = 0;
+        //System.out.println("New RBCs: " + AgeBins[bin0].RBCs);
+        // Old (100 to 120 days old) RBCs die
+        var start_bin = this.bin0 + Blood.HUNDREDDAYS;
+        if( start_bin > Blood.MAXAGE ) start_bin -= (Blood.MAXAGE + 1);
+        //System.out.println("Old RBCs Die");
+        for(var i = 0; i < (Blood.MAXAGE-Blood.HUNDREDDAYS); i++) {
+        	var j = start_bin = i;
+        	if (j < 0) {
+        		this.SimCtlObject.time_stamp();
+        		console.log(" RBC bin value negative " + j);
+        		System.exit(-1);
+        	}
+        	if (j > Blood.MAXAGE) j -= (Blood.MAXAGE + 1);
+            var kill_rate = (i)/((Blood.MAXAGE-Blood.HUNDREDDAYS));
+            this.AgeBins[j].RBCs *= (1.0 - kill_rate);
+            this.AgeBins[j].glycatedRBCs *= (1.0 - kill_rate);
+            //System.out.println("bin: " + (start_bin + i) + ", RBCs " + AgeBins[start_bin + i].RBCs + ", Glycated RBCs " + AgeBins[start_bin + i].glycatedRBCs);
+        }
+        //glycate the RBCs
+        var glycation_prob = this.avgBGLOneDay * this.glycationProbSlope_ + this.glycationProbConst_;
+        //System.out.println("RBCs glycate");
+        for(var i = 0; i <= Blood.MAXAGE; i++) {
+            var newly_glycated = glycation_prob * this.AgeBins[i].RBCs;
+            this.AgeBins[i].RBCs -= newly_glycated;
+            this.AgeBins[i].glycatedRBCs += newly_glycated;
+            //System.out.println("bin: " + i + ", RBCs " + AgeBins[i].RBCs + ", Glycated RBCs " + AgeBins[i].glycatedRBCs);
+        }
+        this.SimCtlObject.time_stamp();
+        console.log("New HbA1c: " + currentHbA1c());
+    }
+
+    
+        // Constructor
+    constructor(myBody) {
+    	this.body = myBody;
+    	this.AgeBins = Array.apply(null, Array(Blood.MAXAGE+1)).map(function () { return new RBCBin(); });
+    	 //tracking RBCs
+        this.bin0 = 1;
+        this.rbcBirthRate_ = 144.0*60*24; // in millions per minute
+        this.glycationProbSlope_ = 0.085/10000.0;
+        this.glycationProbConst_ = 0;
+        this.SimCtlObject = new SimCtl();
+        // all contents are in units of milligrams of glucose
+        this.glucose = 5000.0; //5000.0; //15000.0;
+        this.fluidVolume_ = 50.0; // in deciliters
+        
+        this.gngSubstrates = 0;
+        this.alanine = 0;
+        this.branchedAminoAcids = 0;
+        this.unbranchedAminoAcids = 0;
+        this.glutamine = 0;
+        this.insulin = 0;
+        
+        //Gerich: insulin dependent: 1 to 5 micromol per kg per minute
+        this.glycolysisMin_ = 0.1801559;
+        this.glycolysisMax_ = 5*glycolysisMin_;
+        
+        this.normalGlucoseLevel_ = 100; //mg/dl
+        this.highGlucoseLevel_ = 200; //mg/dl
+        this.minGlucoseLevel_ = 40; //mg/dl
+        this.highLactateLevel_ = 4053.51; // mg
+        // 9 mmol/l of lactate = 4.5 mmol/l of glucose = 4.5*180.1559*5 mg of glucose = 4053.51mg of glucose
+        this.lactate = 0; //450.39; //mg
+        // 1mmol/l of lactate = 0.5mmol/l of glucose = 0.5*180.1559*5 mg of glucose = 450.39 mg of glucose
+
+        // initial number of RBCs
+        for(var i = 0; i <= Blood.MAXAGE; i++)
+        {
+        	this.AgeBins[i] = new RBCBin();
+            this.AgeBins[i].RBCs = 0.94*rbcBirthRate_;
+            this.AgeBins[i].glycatedRBCs = 0.06*rbcBirthRate_;
+        }
+        this.avgBGLOneDay = 0;
+        this.avgBGLOneDaySum = 0;
+        this.avgBGLOneDayCount = 0;
+    }
+    
+    processTick() {
+	    var x; // 
+	    var scale = (1.0 - this.body.insulinResistance_)*(this.body.blood.insulin);
+	    
+	    x = poissonProcess.sample(100.0*this.glycolysisMin_);
+	    x = x*(this.body.bodyWeight_)/100.0;
+	    
+	    if( x > this.glycolysisMax_*(this.body.bodyWeight_))
+	        x = this.glycolysisMax_*(this.body.bodyWeight_);
+	    
+	    var toGlycolysis = x + scale * ( (this.glycolysisMax_*(this.body.bodyWeight_)) - x);
+	    
+	    if( toGlycolysis > this.glucose) toGlycolysis = this.glucose;
+	    
+	    this.glucose -= toGlycolysis;
+	    this.body.blood.lactate += toGlycolysis;
+	    //System.out.println("Glycolysis in blood, blood glucose " + glucose + " mg, lactate " + lactate + " mg")
+	    
+	    var bgl = this.glucose/this.fluidVolume_;
+	    
+	    if( bgl >= this.highGlucoseLevel_)
+	        this.insulin = this.body.insulinPeakLevel_;
+	    else
+	    {
+	        if( bgl < this.normalGlucoseLevel_)
+	            this.insulin = 0;
+	        else
+	        {
+	            this.insulin = (this.body.insulinPeakLevel_)*(bgl - this.normalGlucoseLevel_)/(this.highGlucoseLevel_ - this.normalGlucoseLevel_);
+	        }
+	    }
+	    
+	  //calculating average bgl during a day
+	    
+	    if( this.avgBGLOneDayCount == Blood.ONEDAY )
+	    {
+	        this.avgBGLOneDay = this.avgBGLOneDaySum/this.avgBGLOneDayCount;
+	        this.avgBGLOneDaySum = 0;
+	        this.avgBGLOneDayCount = 0;
+	        updateRBCs();
+	        SimCtlObject.time_stamp();
+	        console.log(" Blood::avgBGL " + this.avgBGLOneDay);
+	    }
+	    
+	    this.avgBGLOneDaySum += bgl;
+	    this.avgBGLOneDayCount++;
+	    
+	    SimCtlObject.time_stamp();
+	    console.log("Blood:: bgl " + getBGL());
+	    
+	    //BUKET NEW: For the calculation of Incremental AUC
+	    //if(glcs > 100 && SimCtl::ticks < 120){
+	    //  SimCtl::AUC = SimCtl::AUC + (glcs-100);
+	    //}
+    }
+    
+    setParams(){
+        for(var [key, value] of this.body.metabolicParameters.get(body.bodyState.state).get(BodyOrgan.BLOOD.value).entries()) {
+            switch (key) {
+    			case "rbcBirthRate_" : { this.rbcBirthRate_ = value; break; }
+    			case "glycationProbSlope_" : { this.glycationProbSlope_ = value; break; }
+    			case "glycationProbConst_" : { this.glycationProbConst_ = value; break; }
+    			case "minGlucoseLevel_" : { this.minGlucoseLevel_ = value; break; }
+    			case "normalGlucoseLevel_" : { this.normalGlucoseLevel_ = value; break; }
+    			case "highGlucoseLevel_" : { this.highGlucoseLevel_ = value; break; }
+    			case "highLactateLevel_" : { this.highLactateLevel_ = value; break; }
+    			case "glycolysisMin_" : { this.glycolysisMin_ = value; break; }
+    			case "glycolysisMax_" : { this.glycolysisMax_ = value; break; }
+    		}
+    	}
+    }
+    
+    removeGlucose(howmuch) {
+    	 this.glucose -= howmuch;
+    	//System.out.println("Glucose consumed " + howmuch + " ,glucose left " + glucose);
+	    if (getBGL() <= this.minGlucoseLevel_) {
+	        SimCtl.time_stamp();
+	        console.log(" bgl dips to: " + getBGL());
+	        System.exit(-1);
+	    }
+    }
+    addGlucose(howmuch) {
+    	this.glucose += howmuch;
+	    //SimCtl.time_stamp();
+	    //System.out.println(" BGL: " + getBGL());
+    }
+    
+    getBGL() { return this.glucose/this.fluidVolume_; }
+
+    getGNGSubstrates(){ 
+    	return (this.gngSubstrates + this.lactate + this.alanine + this.glutamine);
+    }
+    
+    consumeGNGSubstrates(howmuch) {
+    	var total = this.gngSubstrates + this.lactate + this.alanine + this.glutamine;
+	    if( total < howmuch ) {
+	        this.gngSubstrates = 0;
+	        this.lactate = 0;
+	        this.alanine = 0;
+	        this.glutamine = 0;
+	        return total;
+	    }
+	    var factor = (total - howmuch)/total;
+	    this.gngSubstrates *= factor;
+	    this.lactate *= factor;
+	    this.alanine *= factor;
+	    this.glutamine *= factor;
+	    return howmuch;
+    }
+    
+    gngFromHighLactate(rate_) {
+    	// Gluconeogenesis will occur even in the presence of high insulin in proportion to lactate
+    	// concentration. High lactate concentration (e.g. due to high glycolytic activity) would 
+    	// cause gluconeogenesis to happen even if insulin concentration is high. But then 
+    	// Gluconeogenesis would contribute to glycogen store of the liver (rather than generating glucose).
+    	
+    	// rate_ is in units of mg per kg per minute
+	    var x = 3*rate_ * this.lactate/this.highLactateLevel_;
+	    
+	    if( x > this.lactate ) x = this.lactate;
+	    
+	    this.lactate -= x;
+	    return x;
+    }
+}
+ Blood.ONEDAY = 24*60;
+ Blood.MAXAGE = 120*24*60;
+ Blood.HUNDREDDAYS = 100;
---
+++ EXP5/Kidney.js
@@ -0,0 +1,135 @@
+//package sim;
+//import java.util.Map.Entry;
+
+//import org.apache.commons.math3.distribution.PoissonDistribution;
+
+//import enums.BodyOrgan;
+
+class Kidney {
+    
+    constructor(myBody){
+    	this.body = myBody;
+        
+        this,glutamineConsumed_ = 0;
+        
+        this.glucose = 0;
+        this.fluidVolume_ = 1.5; //dl
+        
+        this.gluconeogenesisRate_ = 1.8*0.45038975;
+        this.gngFromLactateRate_ = this.gluconeogenesisRate_; // by default
+        
+          
+        this.Glut2VMAX_ = 5; // mg per kg per minute
+        this.Glut2Km_ = 20*180.1559/10.0; // mg/deciliter equal to 20 mmol/l (Frayn Table 2.2.1)
+        this.Glut1Rate_ = 1; // mg per kg per minute
+        
+        //Gerich: insulin dependent: 1 to 5 micromol per kg per minute
+        this.glycolysisMin_ = 0.1801559; // mg per kg per minute
+        this.glycolysisMax_ = 5*this.glycolysisMin_;
+        
+        this.reabsorptionThreshold_ = 11*180.1559/10; //mg/dl equivalent of 11 mmol/l
+        this.glucoseExcretionRate_ = 100/(11*180.1559/10); // mg per minute per(mg/dl)
+        // As BGL increases from 11 mmol/l to 22 mmol/l, glucose excretion in urine increases from 0 to mg/min to 100mg/min.
+    }
+    
+    processTick() {
+        var x; // to hold the random samples
+        x = this.body.bodyWeight_;
+        
+        
+        var bgl = this.body.blood.getBGL();
+        var glInKidney = this.glucose/this.fluidVolume_;
+        
+        x = poissonProcess.sample(x*this.Glut2VMAX_);
+        var y = poissonProcess.sample(x*this.Glut1Rate_);
+        
+        if( glInKidney < bgl ) {
+            //BUKET NEW: In addition to increased glucose production, renal glucose uptake is increased in both the post-absorptive and postprandial
+        	//states in patientswithT2DM[45,46]. So it depends on insulin resistance. (Gerich paper)
+            
+            var diff = bgl - glInKidney;
+            var g = (1 + this.body.insulinResistance_)*x*diff/(diff + this.Glut2Km_);
+            // uptake increases for higher insulin resistance.
+            // may want to change this formula later - Mukul
+            g += y; // basal absorption
+            
+            this.body.blood.removeGlucose(g);
+            this.glucose += g;
+            //System.out.println("Kidney removing " + g + " mg of glucose frm blood");
+        }
+        else
+        {
+            var diff = glInKidney - bgl;
+            var g = (1 + this.body.insulinResistance_)*x*diff/(diff + this.Glut2Km_);
+            
+            if( g > this.glucose )
+            {
+                console.log("Releasing more glucose to blood than what is present in liver!");
+                System.exit(-1); ///=======================================================================
+            }
+            
+            this.glucose -= g;
+            this.body.blood.addGlucose(g);
+            console.log("Kidney releasing " + g + " mg of glucose to blood");
+        }
+                
+        var scale = (1.0 - this.body.insulinResistance_)*(this.body.blood.insulin);
+        
+        x = poissonProcess.sample(x*this.glycolysisMin_);
+        if( x > this.glycolysisMax_*(this.body.bodyWeight_)) 
+        	x = this.glycolysisMax_*(this.body.bodyWeight_);
+        
+        var toGlycolysis = x + scale* ( (this.glycolysisMax_*(this.body.bodyWeight_)) - x);
+        
+        if( toGlycolysis > this.glucose)
+            toGlycolysis = this.glucose;
+        this.glucose -= toGlycolysis;
+        this.body.blood.lactate += toGlycolysis;
+       
+        scale = 1 - (this.body.blood.insulin)*(1 - (this.body.insulinResistance_));
+        x = poissonProcess.sample(x*this.gluconeogenesisRate_);
+    
+        var gng = x *scale;
+        this.glucose += this.body.blood.consumeGNGSubstrates(gng);
+        console.log("GNG in Kidney " + gng);
+        
+        
+        x =  poissonProcess.sample(x*this.gngFromLactateRate_);
+        this.glucose += this.body.blood.gngFromHighLactate(x);
+        
+        console.log("GNG from lactate " + x);
+        
+        if( this.body.blood.glutamine > this.glutamineConsumed_ ) {
+            this.body.blood.glutamine -= this.glutamineConsumed_;
+        } else {
+            this.body.blood.glutamine = 0;
+        }
+        
+        
+        bgl = this.body.blood.getBGL();
+
+        if( bgl > reabsorptionThreshold_ )
+        {
+            x = poissonProcess.sample(this.glucoseExcretionRate_);
+            var g = x*(bgl-reabsorptionThreshold_);
+            this.body.blood.removeGlucose(g);
+            console.log("glucose excretion in urine " + g);
+        }
+    
+    }
+    
+    setParams() {
+    	for(var [key, value] of this.body.metabolicParameters.get(this.body.bodyState.state).get(BodyOrgan.KIDNEY.value).entries()) {
+            switch (key) {
+			    case "fluidVolume_" : { this.fluidVolume_ = value; break; }
+    			case "Glut2VMAX_" : { this.Glut2VMAX_ = value; break; }
+    			case "Glut2Km_" : { this.Glut2Km_ = value; break; }
+    			case "Glut1Rate_" : { this.Glut1Rate_ = value; break; }
+    			case "glycolysisMin_" : { this.glycolysisMin_ = value; break; }
+    			case "glycolysisMax_" : { this.glycolysisMax_ = value; break; }
+    			case "gluconeogenesisRate_" : { this.gluconeogenesisRate_ = value; break; }
+    			case "glutamineConsumed_" : { this.glutamineConsumed_ = value; break; }
+    		}
+    	}
+    }
+}
