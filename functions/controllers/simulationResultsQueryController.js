b5780c6db5755e8403dae52e7e94f5a5319e4b57
cegebran <cegebran@gmail.com>
2018-04-23 18:17:38 -0500

    Added simulationResultsQueryController

---
+++ functions/controllers/simulationResultsQueryController.js
@@ -0,0 +1,11 @@
+var firebase = require('firebase');
+var async = require('async');
+
+exports.querySimulation_get = function(req, res) {
+
+}
+
+exports.querySimulation_post = function(req, res) {
+
+}
+
