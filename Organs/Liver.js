5c61dd78365c6d944fd0bbaeed05e0401fed37e9
powells39 <powells@uwm.edu>
2018-04-25 03:03:28 -0500

    Add files via upload
    Lots of format changes and additional statements to better reflect the newest c++ version.

--- Organs/Liver.js
+++ Organs/Liver.js
@@ -32,6 +32,13 @@ class Liver {
 	    this.glucose = this.normalGlucoseLevel_*this.fluidVolume_;
 	    this.Glut2Km_ = 20*180.1559/10.0; // mg/deciliter equal to 20 mmol/l (Frayn Table 2.2.1)
 	    this.Glut2VMAX_ = 50; //mg per kg per minute
+
+        this.absorptionPerTick;
+        this.toGlycogenPerTick;
+        this.fromGlycogenPerTick;
+        this.toGlycolysisPerTick;
+        this.gngPerTick;
+        this.releasePerTick;
     }
     
     //Call private methods
@@ -66,32 +73,46 @@ class Liver {
             
             this.body.portalVein.removeGlucose(g);
             this.glucose += g;
+            this.absorptionPerTick = g;
             console.log("Liver absorbs from portal vein " + g);
         }
-        
+        //release all portalVein glucose to blood
         this.body.portalVein.releaseAllGlucose();
 
-        
+        // glycogen synthesis (depends on insulin and glucose level)
+
         glInLiver = this.glucose/this.fluidVolume_;
         var scale = this.glInLiver/this.normalGlucoseLevel_;
         //scale *= (1.0 - body.insulinResistance_);
         scale *= this.body.blood.insulin;
         x = poissonProcess.sample(x*this.glucoseToGlycogen_);
-        var toGlycogen = scale * x;
+        var toGlycogen = scale * x * (this.body.bodyWeight)/1000.0;
+
         
         if( toGlycogen > this.glucose )
             toGlycogen = this.glucose;
         
-        this.glycogen += toGlycogen;
-        
+        if( toGlycogen > 0 )
+        {
+            this.glycogen += toGlycogen;
+            //this.body.time_stamp();
+            //console.log(" glycogen synthesis in liver " + toGlycogen + "mg" );
+
+        }
+
+        this.toGlycogenPerTick = toGlycogen;
+
         if( this.glycogen > this.glycogenMax_ )
         {
+        //if the liver cannot store any more glycogen, we assume that this glucose (which would have been stored as glycogen) is converted to fat.
+            //this.body.time_stamp();
+            //console.log(" glucose consumed for Lipogenesis in liver " + glycogen - glycogenMax_ + "mg");
             this.body.adiposeTissue.lipogenesis(this.glycogen - this.glycogenMax_);
             this.glycogen = this.glycogenMax_;
         }
         
        
-        this.glucose -= toGlycogen;
+        this.glucose -= this.toGlycogen;
         
         //console.log("After glycogen synthesis in liver, liver glycogen " + glycogen + " mg, live glucose " + glucose + " mg");
         
@@ -100,23 +121,37 @@ class Liver {
         scale = 1 - (this.body.blood.insulin)*(1 - (this.body.insulinResistance_));
         glInLiver = this.glucose/this.fluidVolume_;
         
-        if( glInLiver > this.normalGlucoseLevel_ ) {
+        if( glInLiver > this.normalGlucoseLevel_ ) 
+        {
             scale *= this.normalGlucoseLevel_/glInLiver;
         }
         
         x = poissonProcess.sample(x*this.glycogenToGlucose_);
-        var fromGlycogen = scale * x;
+        var fromGlycogen = scale * x * (this.body.bodyWeight)/1000.0;
+
         
         if( fromGlycogen > this.glycogen )
             fromGlycogen = this.glycogen;
         
+        if( fromGlycogen > 0 )
+        {
         this.glycogen -= fromGlycogen;
         this.glucose += fromGlycogen;
         
-        
+        //this.body.time_stamp();
+        //console.log(" glycogen breakdown in liver " + fromGlycogen + "mg" );
+        }
+        this.fromGlycogenPerTick = fromGlycogen;
+
+        //console.log("After glycogen breakdown in liver, liver glycogen " + glycogen + " mg, liver glucose " + glucose + " mg, blood glucose " + body.blood.glucose + " mg, blood lactate " + body.blood.lactate + " mg" );
+        //Glycolysis. Depends on insulin level. Some of the consumed glucose becomes lactate.
+    
+        //Gerich paper: Liver consumes 1.65 micomol per kg per minute to 16.5 micomol per kg per minute of glucose depending upon post-absorptive/post-prandial state.
+    
         scale = (1.0 - this.body.insulinResistance_)*(this.body.blood.insulin);
         
         x =  poissonProcess.sample(x*this.glycolysisMin_);
+        x *= (body.bodyWeight)/1000.0;
         if( x > this.glycolysisMax_*(this.body.bodyWeight_))
             x = this.glycolysisMax_*(this.body.bodyWeight_);
 
@@ -126,36 +161,78 @@ class Liver {
             toGlycolysis = this.glucose;
         this.glucose -= toGlycolysis;
         this.body.blood.lactate += toGlycolysis*this.glycolysisToLactateFraction_;
+        this.toGlycolysisPerTick = toGlycolysis;
+
+        //this.body.time_stamp();
+        //console.log(" glycolysis in liver " + toGlycolysis + "mg");
+        //console.log("After glycolysis , liver glucose " + glucose + " mg, blood lactate " + this.body.blood.lactate + " mg";
+    
+        //gluconeogenesis. Depends on insulin level and on substrate concentration.
+
         scale = 1 - (this.body.blood.insulin)*(1 - (this.body.insulinResistance_));
         x = poissonProcess.sample(x*this.gluconeogenesisRate_);
-        var gng = x *scale;
-        this.glucose += this.body.blood.consumeGNGSubstrates(gng);
+        var gng = x *scale * (this.body.bodyWeight)/1000.0;
+        gng = this.body.blood.consumeGNGSubstrates(gng);
+        if( gng > 0 )
+        {
+        glucose += gng;
+        //this.body.time_stamp();
+        //console.log(" gng in liver " + gng + "mg");
+        }
+        this.gngPerTick = gng;
         
         //Gluconeogenesis will occur even in the presence of high insulin in proportion to lactate concentration. High lactate concentration (e.g. due to high glycolytic activity) would cause gluconeogenesis to happen even if insulin concentration is high. But then Gluconeogenesis would contribute to glycogen store of the liver (rather than generating glucose).
+        
         x = poissonProcess.sample(x*this.gngFromLactateRate_);
+        x *= (this.body.bodyWeight)/1000.0;
         this.glycogen += this.body.blood.gngFromHighLactate(x);
+        if( x > 0 )
+        {
+            this.glucose += x;
+            //this.body.time_stamp();
+            //console.log(" gng in liver from high lactate " + x + "mg");
+        }
+        this.gngPerTick += x;
+    
         //console.log("After GNG , liver glucose " + glucose + " mg, liver glycogen " + glycogen + " mg, blood glucose " + body.blood.glucose + " mg, blood lactate " + body.blood.lactate + " mg");
               
-        console.log(this.body.portalVein.releaseAminoAcids());
+        //BUKET NEW: 93% of unbranched amino acids in portal vein are retained in Liver, because the leaked amino acids from Intestine consists of 15% branched and 85% unbranched, but after liver consumption the percentage needs to be 70% branched, 30% unbranched. To provide these percentages 93% of unbranched amino acids in portal vein are retained in liver. (From Frayn's book)
+    
+        body.portalVein.releaseAminoAcids();
         
+        //6. consume some glucose to form fatty acids (at configured rate that depends on glucose level)
+
+        /*
+        if( body.blood.glucose > body.blood.normalGlucoseLevel_){
+            body.blood.consumeGlucose(glucoseToNEFA_);
+        }
+        */
+
         glInLiver = this.glucose/this.fluidVolume_;
-        console.log( this.fluidVolume_);
         var bgl = this.body.blood.getBGL();
         
-        if( glInLiver > bgl ) {
+        this.releasePerTick = 0;
+
+        if( glInLiver > bgl ) 
+        {
             var diff = glInLiver - bgl;
             x = poissonProcess.sample(x*this.Glut2VMAX_);
+            x *= (body->bodyWeight)/1000.0;
             var g = x*diff/(diff + this.Glut2Km_);
         
             if( g > this.glucose ) {
                 console.log("Releasing more glucose to blood than what is present in liver!");
                 System.exit(-1);
             }
+
             this.glucose -= g;
             this.body.blood.addGlucose(g);
+            this.releasePerTick = g;
+
             this.body.time_stamp();
-            console.log("Liver released glucose " + g);
+            console.log("Liver released glucose " + g + "mg to blood");
         }
+        
         //this.body.time_stamp();
          console.log(" Liver:: " + this.glycogen + " " + this.glucose + " " + this.glucose/this.fluidVolume_);
     }
