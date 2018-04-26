9f29b3fd5e140943c2d8aa921b0b3c77d937f3b4
AlexYang1994 <yaajalex@gmail.com>
2018-04-26 10:39:11 -0500

    Update Heart.js

--- EXP7 FINAL/Test 31/Heart.js
+++ EXP7 FINAL/Test 31/Heart.js
@@ -13,27 +13,40 @@ class Heart {
         
         this.Glut4Km_ = 5*180.1559/10.0; //mg/dl equivalent of 5 mmol/l
         this.Glut4VMAX_ = 0; //mg per kg per minute
+        this.oxidationPerTick;
     }
     
     processTick() {
         
-        var basalAbsorption = poissonProcess(this.basalGlucoseAbsorbed_, this.body.MyEngine());
+        var basalAbsorption = poissonProcess(this.basalGlucoseAbsorbed_);
         this.body.blood.removeGlucose(basalAbsorption);
         
+        //var lactateOxidized__ = poissonProcess(1000 * this.lactateOxidized_);
+        
+        this.body.blood.removeGlucose(basalAbsorption);
+        
+        this.oxidationPerTick = basalAbsorption;
+        
+        //Absorption via GLUT4
         
         var bgl = this.body.blood.getBGL();
         var scale = (1.0 - this.body.insulinResistance_)*(this.body.blood.insulin)*(this.body.bodyWeight_);
         var g = scale*this.Glut4VMAX_*bgl/(bgl + this.Glut4Km_);
         
         this.body.blood.removeGlucose(g);
+        
+        this.oxidationPerTick += g;
+        
+        this.body.time_stamp();
 
+        /*
         if( this.body.blood.lactate >= this.lactateOxidized_ ) {
             this.body.blood.lactate -= this.lactateOxidized_;
         } else {
             this.body.blood.lactate = 0;
         }
-
-        console.log("Working");
+        */
+        console.log("Heart:: Oxidation " + oxidationPerTick);
         
     }
     setParams() {
