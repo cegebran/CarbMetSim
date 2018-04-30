58e4e2749fb8774f91314a66a3d6b4d61220caaf
AlexYang1994 <yaajalex@gmail.com>
2018-04-30 17:24:11 -0500

    Update Liver.js

--- EXP7 FINAL/Test 31/Liver.js
+++ EXP7 FINAL/Test 31/Liver.js
@@ -4,157 +4,267 @@
 
 //import org.apache.commons.math3.distribution.PoissonDistribution;
 
-//import enums.BodyOrgan;
-
 class Liver {
-
     constructor(body_) {
-    	this.body = body_;
-	    this.glycogen = 100000.0; // equivalent of 100g of glucose
-	    this.glycogenMax_ = 120000.0; // 120 g of glucose
-	    
-	    this.glycogenToGlucose_ = 2*0.9007795;
-	    this.glucoseToGlycogen_ =this.glycogenToGlucose_; // for now
+        this.body = body_;
+        this.glycogen = 100000.0; // equivalent of 100g of glucose
+        this.glycogenMax_ = 120000.0; // 120 g of glucose
 
-	    this.glycolysisMin_ = 0.297; //mg per kg per minute
-	    this.glycolysisMax_ = 2.972;
-	    
-	    this.glycolysisToLactateFraction_ = 1; // by default glycolysis just generates all lactate
-	    
-	    // 2.5 micromol per kg per minute = 2.5*180.1559/1000 mg per kg per minute = 0.45038975 mg per kg per minute
-	    this.gluconeogenesisRate_ = 1.8*0.45038975;
-	    this.gngFromLactateRate_ = this.gluconeogenesisRate_; //by default
-	    
-	    this.glucoseToNEFA_ = 0;
-	    
-	    this.normalGlucoseLevel_ = 100; //mg/dl
-	    this.fluidVolume_ = 10; //dl
-	    this.glucose = this.normalGlucoseLevel_*this.fluidVolume_;
-	    this.Glut2Km_ = 20*180.1559/10.0; // mg/deciliter equal to 20 mmol/l (Frayn Table 2.2.1)
-	    this.Glut2VMAX_ = 50; //mg per kg per minute
+        // Frayn Chapter 9
 
+        // 5 micromol per kg per minute = 5*180.1559/1000 mg per kg per minute = 0.9007795 mg per kg per minute (ref: Gerich paper)
+        // default max glycogen breakdown rate is 10 micromol per kg per minute
+        this.glycogenToGlucose_ = 2*0.9007795;
+        this.glucoseToGlycogen_ = this.glycogenToGlucose_; // for now
+
+        this.glycogenSynth_Insulin_Mean_ = 0.075;
+        this.glycogenSynth_Insulin_StdDev_ = 0.02;
+
+        //Gerich paper: Liver consumes 1.65 micromol per kg per minute to 16.5 micromol per kg per minute of glucose depending upon post-absorptive/post-prandial state.
+        this.glycolysisMin_ = 0.297; //mg per kg per minute
+        this.glycolysisMax_ = 2.972;
+
+        this.glycolysisToLactateFraction_ = 1; // by default glycolysis just generates all lactate
+
+        // 2.5 micromol per kg per minute = 2.5*180.1559/1000 mg per kg per minute = 0.45038975 mg per kg per minute
+        // default gng rate is 5 micromol per kg per minute
+        this.gluconeogenesisRate_ = 2.0*0.45038975;
+        this.gngFromLactateRate_ = 9 * this.gluconeogenesisRate_; //by default
+
+        this.glucoseToNEFA_ = 0;
+
+        this.fluidVolume_ = 10; //dl
+        this.glucose = 100 * this.fluidVolume_; // assuming glucose concentration to be 100mg/dl
+
+        this.Glut2Km_ = 20*180.1559/10.0; // mg/deciliter equal to 20 mmol/l (Frayn Table 2.2.1)
+        this.Glut2VMAX_ = 50; //mg per kg per minute
+        
+        
+        this.absorptionPerTick;
+        this.toGlycogenPerTick;
+        this.fromGlycogenPerTick;
+        this.glycolysisPerTick;
+        this.gngPerTick;
+        this.releasePerTick;
     }
     
-    //Call private methods
+    erf(x){
+        // erf(x) = 2/sqrt(pi) * integrate(from=0, to=x, e^-(t^2) ) dt
+        // with using Taylor expansion, 
+        //        = 2/sqrt(pi) * sigma(n=0 to +inf, ((-1)^n * x^(2n+1))/(n! * (2n+1)))
+        // calculationg n=0 to 50 bellow (note that inside sigma equals x when n = 0, and 50 may be enough)
+        var m = 1.00;
+        var s = 1.00;
+        var sum = x * 1.0;
+        for(var i = 1; i < 50; i++){
+            m *= i;
+            s *= -1;
+            sum += (s * Math.pow(x, 2.0 * i + 1.0)) / (m * (2.0 * i + 1.0));
+        }  
+        return 2 * sum / Math.sqrt(3.14159265358979);
+    }
+    
     processTick() {
-    	// every thing is stochastic
-        var x; // to hold the random samples
-        x = this.body.bodyWeight_;
-        // Now do the real work
+    	var baseBGL = this.body.blood.baseBGL();
         
+        var x;  // to hold the random samples
+       
+        var glycogenToGlucose__ = poissonProcess.sample(1000.0 * this.glycogenToGlucose_);
+        
+        var glucoseToGlycogen__ = poissonProcess.sample(1000.0 * this.glucoseToGlycogen_);
+        
+        var glycolysisMin__ = poissonProcess.sample(1000.0 * this.glycolysisMin_);
+        
+        var gngRate__ = poissonProcess.sample(1000.0 * this.gluconeogenesisRate_);
+        
+        var gngFromLactateRate__ = poissonProcess.sample(1000.0 * this.gngFromLactateRate_);
+        
+        var Glut2VMAX__ = poissonProcess.sample(1000.0 * this.Glut2VMAX_);
+       
         var glInPortalVein = this.body.portalVein.getConcentration();
 
         var glInLiver = this.glucose/this.fluidVolume_;
       
-        
         if( glInLiver < glInPortalVein ) {
             var diff = glInPortalVein - glInLiver;
-            x =  poissonProcess.sample(x*this.Glut2VMAX_);
+            x = Glut2VMAX__;
+            x *= this.body.bodyWeight_ / 1000;
+            
             var g = x * diff/(diff + this.Glut2Km_);
             
             if( g > this.body.portalVein.getGlucose() ) {
-                //System.out.println("Trying to absorb more glucose from portal vein than what is present there! " + g + " " + body.portalVein.getGlucose());
+                //console.log("Trying to absorb more glucose from portal vein than what is present there! " + g + " " + this.body.portalVein.getGlucose());
                 g = this.body.portalVein.getGlucose();
             }
             
             this.body.portalVein.removeGlucose(g);
             this.glucose += g;
-            console.log("Liver absorbs from portal vein " + g);
+            this.absorptionPerTick = g;
+            
+            this.body.time_stamp();
+            console.log("Liver absorbs from portal vein " + g + "mg");
         }
         
         this.body.portalVein.releaseAllGlucose();
 
-        
         glInLiver = this.glucose/this.fluidVolume_;
-        var scale = glInLiver/this.normalGlucoseLevel_;
-        console.log("scale"+ glInLiver)
-        //scale *= (1.0 - body.insulinResistance_);
-        scale *= this.body.blood.insulin;
+        var scale = 1;
+        
+        if(glInLiver > baseBGL){
+            scale *= glInLiver / baseBGL;
+        }
+        
+        scale *= (1.0 - this.body.insulinResistance_);
+        //scale *= this.body.blood.insulin;
+        
+        scale *= .5 * (1 + erf((this.body.blood.insulin - this.glycogenSynth_Insulin_Mean_) / (this.glycogenSynth_Insulin_StdDev_ * Math.sqrt(2))));
 
-
-
-        x = poissonProcess.sample(x*this.glucoseToGlycogen_);
-        var toGlycogen = scale * x;
+        x = glucoseToGlycogen__;
+        var toGlycogen = scale * x * (this.body.bodyWeight_ / 1000);
+        
         if( toGlycogen > this.glucose )
             toGlycogen = this.glucose;
         
-        this.glycogen += toGlycogen;
-       
+        if( toGlycogen > 0 )
+        {
+            this.glycogen += toGlycogen;
+        }
+        
+        this.toGlycogenPerTick = toGlycogen;
+        
         if( this.glycogen > this.glycogenMax_ )
         {
             this.body.adiposeTissue.lipogenesis(this.glycogen - this.glycogenMax_);
             this.glycogen = this.glycogenMax_;
         }
         
-       
         this.glucose -= toGlycogen;
         
-        //System.out.println("After glycogen synthesis in liver, liver glycogen " + glycogen + " mg, live glucose " + glucose + " mg");
+        //console.log("After glycogen synthesis in liver, liver glycogen " + this.glycogen + " mg, live glucose " + this.glucose + " mg");
         
         //glycogen breakdown (depends on insulin and glucose level)
         
         scale = 1 - (this.body.blood.insulin)*(1 - (this.body.insulinResistance_));
         glInLiver = this.glucose/this.fluidVolume_;
         
-        if( glInLiver > this.normalGlucoseLevel_ ) {
-            scale *= this.normalGlucoseLevel_/glInLiver;
+        if( glInLiver > baseBGL )
+        {
+            scale *= baseBGL/glInLiver;
         }
         
-        x = poissonProcess.sample(x*this.glycogenToGlucose_);
-        var fromGlycogen = scale * x;
+        x = glycogenToGlucose__;
+        var fromGlycogen = scale * x * (this.body.bodyWeight_ / 1000);
         
-        if( fromGlycogen > this.glycogen )
+        if( fromGlycogen > this.glycogen ){
             fromGlycogen = this.glycogen;
+        }
         
-        this.glycogen -= fromGlycogen;
-        this.glucose += fromGlycogen;
-               
+        if( fromGlycogen > 0 )
+        {
+            this.glycogen -= fromGlycogen;
+            this.glucose += fromGlycogen;
+        }
         
-        scale = (1.0 - this.body.insulinResistance_)*(this.body.blood.insulin);
+        this.fromGlycogenPerTick = fromGlycogen;
         
-        x =  poissonProcess.sample(x*this.glycolysisMin_);
-        if( x > this.glycolysisMax_*(this.body.bodyWeight_))
-            x = this.glycolysisMax_*(this.body.bodyWeight_);
-
-        var toGlycolysis = x + scale* ( (this.glycolysisMax_*(this.body.bodyWeight_)) - x);
+        scale = (1 - this.body.insulinResistance_) * this.body.blood.insulin;
         
-        if( toGlycolysis > this.glucose)
+        x = glycolysisMin__;
+        x *= this.body.bodyWeight_ / 1000;
+        
+        if(x > this.glycolysisMax_ * this.body.bodyWeight_){
+            x = this.glycolysisMax_ * this.body.bodyWeight_;
+        }
+        
+        var toGlycolysis = x + scale * ((this.glycolysisMax_ * this.body.bodyWeight_) - x);
+        
+        if( toGlycolysis > this.glucose){
             toGlycolysis = this.glucose;
+        }
+        
         this.glucose -= toGlycolysis;
-        this.body.blood.lactate += toGlycolysis*this.glycolysisToLactateFraction_;
-        scale = 1 - (this.body.blood.insulin)*(1 - (this.body.insulinResistance_));
-        x = poissonProcess.sample(x*this.gluconeogenesisRate_);
-        var gng = x *scale;
-        this.glucose += this.body.blood.consumeGNGSubstrates(gng);
+        this.body.blood.lactate += toGlycolysis * this.glycolysisToLactateFraction_;
         
-        //Gluconeogenesis will occur even in the presence of high insulin in proportion to lactate concentration. High lactate concentration (e.g. due to high glycolytic activity) would cause gluconeogenesis to happen even if insulin concentration is high. But then Gluconeogenesis would contribute to glycogen store of the liver (rather than generating glucose).
-        x = poissonProcess.sample(x*this.gngFromLactateRate_);
-        this.glycogen += this.body.blood.gngFromHighLactate(x);
+        this.glycolysisPerTick = toGlycolysis;
+            
+        scale = 1 - this.body.blood.insulin * (1 - this.body.insulinResistance_);
+        x = gngRate__;
+        
+        var gng = this.body.blood.consumeGNGSubstrates(gng);
+        
+        if( gng > 0 )
+        {
+            this.glucose += gng;
+            //
+            //this.body.time_stamp();
+            //console.log("gng in liver " + gng + "mg");
+        }
+        
+        this.gngPerTick = gng;
 
-        //System.out.println("After GNG , liver glucose " + glucose + " mg, liver glycogen " + glycogen + " mg, blood glucose " + body.blood.glucose + " mg, blood lactate " + body.blood.lactate + " mg");
-              
-        console.log(this.body.portalVein.releaseAminoAcids());
+         //Gluconeogenesis will occur even in the presence of high insulin in proportion to lactate concentration. 
+
+        x = gngFromLactateRate__;
+        x *= this.body.bodyWeight_ / 1000;
+        x = this.body.blood.gngFromHighLactate(x);
         
-        glInLiver = this.glucose/this.fluidVolume_;
-        console.log( this.fluidVolume_);
+        if(x > 0)
+        {
+            this.glucose += x;
+            //this.body.time_stamp();
+            //console.log("gng in liver from high lactate " + x + "mg");
+        }
+        
+        this.gngPerTick += x;
+        
+        this.body.portalVein.releaseAminoAcids();
+            
+        glInLiver = this.glucose / this.fluidVolume_;
+        
         var bgl = this.body.blood.getBGL();
         
-        if( glInLiver > bgl ) {
-            var diff = glInLiver - bgl;
-            x = poissonProcess.sample(x*this.Glut2VMAX_);
-            var g = x*diff/(diff + this.Glut2Km_);
+        this.releasePerTick = 0;
         
-            if( g > this.glucose ) {
+        if( glInLiver > bgl )
+        {
+            var diff = glInLiver - bgl;
+            x = Glut2VMAX__;
+            x *= this.body.bodyWeight_ / 1000;
+            var g = x*diff/(diff + this.Glut2Km_);
+
+            if( g > this.glucose )
+            {
                 console.log("Releasing more glucose to blood than what is present in liver!");
-                System.exit(-1);
+                exit(-1);
             }
+
             this.glucose -= g;
             this.body.blood.addGlucose(g);
+            this.releasePerTick = g;
             this.body.time_stamp();
-            console.log("Liver released glucose " + g);
+            console.log("Liver released glucose " + g + "mg to blood");
         }
-        //SimCtl.time_stamp();
-         console.log(" Liver:: " + this.glycogen + " " + this.glucose + " " + this.glucose/this.fluidVolume_);
+        
+        this.body.time_stamp();
+        console.log("Liver:: Absorption " + this.absorptionPerTick);
+        
+        this.body.time_stamp();
+        console.log("Liver:: ToGlycogen " + this.toGlycogenPerTick);
+        
+        this.body.time_stamp();
+        console.log("Liver:: FromGlycogen " + this.fromGlycogenPerTick);
+        
+        this.body.time_stamp();
+        console.log("Liver:: glycogen " + this.glycogen);
+        
+        this.body.time_stamp();
+        console.log("Liver:: Glycolysis " + this.glycolysisPerTick);
+        
+        this.body.time_stamp();
+        console.log("Liver:: GNG " + this.gngPerTick);
+        
+        this.body.time_stamp();
+        console.log("Liver:: Release " + this.releasePerTick);
     }
     
     setParams() {
@@ -176,75 +286,3 @@ class Liver {
     	}
     }
 }
-Liver.PortalVein  = class {
-    	
-        constructor(body_) {
-        	this.body = body_;
-            this.glucose = 0; //mg
-            this.branchedAA = 0;	//mg
-            this.unbranchedAA = 0; //mg
-            this.fluidVolume_ = 5; // dl
-        }
-        
-        processTick() {
-        	var bgl = this.body.blood.getBGL();
-            var glucoseFromBlood = bgl*this.fluidVolume_;
-            this.body.blood.removeGlucose(glucoseFromBlood);
-            this.glucose += glucoseFromBlood;
-            
-            //SimCtl.time_stamp();
-        }
-        
-        setParams() {
-            for(var [key, value] of this.body.metabolicParameters.get(this.body.bodyState.state).get(BodyOrgan.PORTAL_VEIN.value).entrySet()) {            
-                switch (key) {
-                    case "fluidVolume_" : { this.fluidVolume_ = value; break; }
-                }
-            }
-        }
-        
-        getConcentration() {
-        	var gl = this.glucose/this.fluidVolume_;
-            //SimCtl.time_stamp();
-            //System.out.println("GL in Portal Vein: " + gl);
-         
-            return gl;
-        }
-        
-        addGlucose(g) {this.glucose += g;}
-        
-        getGlucose(){return this.glucose;}
-        
-        removeGlucose(g) {
-        	this.glucose -= g;
-            if( this.glucose < 0 ) {
-                console.log("PortalVein glucose went negative");
-                System.exit(-1);
-            }
-        }
-        
-        releaseAllGlucose() {
-        	this.body.blood.addGlucose(glucose);
-            this.glucose = 0;
-        }
-        
-        addAminoAcids(aa) {
-        	this.branchedAA += 0.15*aa;
-            this.unbranchedAA += 0.85*aa;
-            //SimCtl.time_stamp();
-            //System.out.println(" PortalVein: bAA " + branchedAA + ", uAA " + unbranchedAA);
-        }
-        
-        releaseAminoAcids() {
-        	// 93% unbranched amino acids consumed by liver to make alanine
-            this.body.blood.alanine += 0.93*this.unbranchedAA;
-            this.body.blood.unbranchedAminoAcids += 0.07*this.unbranchedAA;
-            this.unbranchedAA = 0;
-            this.body.blood.branchedAminoAcids += this.branchedAA;
-            this.branchedAA = 0;
-            // who consumes these amino acids from blood other than liver?
-            // brain consumes branched amino acids
-        }
-
-    }
-
